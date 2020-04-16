f0064fc3217c1e33c4150aaf93445cc0d5af1440
hughgreethead <hugh.greethead@gmail.com>
2020-04-16 10:45:48 +1000

    View controller methods updated

--- app/controllers/celebrities_controller.rb
+++ app/controllers/celebrities_controller.rb
@@ -4,35 +4,53 @@ class CelebritiesController < ApplicationController
     
     #Show all celebrities
     def index
+
     end
     
     #Show a single celebrity
     def show
-        @name = “”
-        @notability = “”
-        @celebrities.each do |celebrity|
-            if params[:name] == celebrity[“name”]
-                @name = celebrity[“name”]
-                @notability = celebrity[“notability”]
-                render “show”
+        puts params["name"]
+        @selected_celebrity = {}
+        for celebrity in celebrities
+            if celebrity["name"] == params["name"]
+                @selected_celebrity = celebrity
             end
         end
     end
     
     #Create a new celebrity
     def create
+        name = params["name"]
+        notability = params["notability"]
+        new_celebrity = {"name" => name, "notability" => notability}
+        @celebrities.push(new_celebrity)
+        render json: new_celebrity
+
     end
     
     #Update a celebrity
     def update
+        puts params["name"]
+        updated_celebrity = {}
+        for celebrity in @celebrities
+            if celebrity["name"] == params["name"]
+                celebrity["notability"] = params["notability"]
+                updated_celebrity = celebrity
+            end
+        end
+        render json: updated_celebrity
     end
     
     #Remove a celebrity
     def destroy
+        @celebrities = @celebrities.select {|celebrity| celebrity["name"] != params["name"]}
+        render plain: ""
+
     end
 
     private
     def setup_data
+        # This data stored in a cookie - unless session - means if there is already data in our session we leave it
         session[:celebrities] = [
             { name: "Lindsay Lohan", notability: "Parent Trap" },
             { name: "Adam Sandler", notability: "Big Daddy" },
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
-  
+  # root index example
   # get "/", to: "celebrities#index", as: "root"
 
+  # Index
   get "/celebrities", to: "celebrities#index"
   # Read
-  get "/celebrities/:id", to: "celebrities#show"
+  get "/celebrities/:name", to: "celebrities#show"
   # Create
   post "/celebrities", to: "celebrities#create"
-  # Update
-  put "/celebrities", to: "celebrities#update"
-  patch "/celebrities", to: "celebrities#update"
-  #Delete
-  delete "/celebrities", to: "celebrities#destroy"
+  # Update - want to update at a specific celebrity
+  put "/celebrities/:name", to: "celebrities#update"
+  #Delete - want to go to a celebrities specific URL to delete
+  delete "/celebrities/:name", to: "celebrities#destroy"
 
 end
